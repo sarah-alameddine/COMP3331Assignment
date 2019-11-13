f8787dab78e4c5ee0552ddd43ad33fc1a690baf4
sarah <z5162766@ad.unsw.edu.au>
2019-11-13 19:23:45 +1100

    read from file

--- client.py
+++ client.py
@@ -1,4 +1,30 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1234))
+s.connect((socket.gethostname(), 2495))
+
+
+
+username = input("Enter Username:")
+s.send(username.encode())
+
+# print from server "enter password 
+msg = s.recv(1024).decode()
+print(msg)
+
+password = input()
+s.send(password.encode())
+
+msg = s.recv(1024).decode()
+print(msg)
+
+
+'''
+while msg.lower().strip() != 'logout':
+	s.send(msg.encode())
+	#s.sendto(message.encode(),(serverName, serverPort))
+		#msg = 
+	msg = input("Enter mesg:")
+
+'''
+
--- server.py
+++ server.py
@@ -1,24 +1,40 @@
 import socket
 
-
+# Dictionary will store all the usernames & passwords 
+userDict = {}
 # A header will store the length of the messeges sent
 
-# userDict
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 1234))
+s.bind((socket.gethostname(), 2495))
 s.listen(5)
 
-username = {
-	"sarah": "123"
-}
 
-while True:
-    # now our endpoint knows about the OTHER endpoint.
-    clientsocket, address = s.accept()
-    print(f"connection from {address} has been established")
-    
-    print(f"Enter Username:")
-    
-    # msg = s.recv(10)
-     
-     clientsocket.recv(10)
+# Open the credential.txt file to store usernames and passwords in dictionary
+# Stores the left column as users (keys) and the right colum as passwords(value)
+with open("Credentials.txt") as f:
+    for line in f:
+       (key, val) = line.split()
+       userDict[key] = val
+
+
+# now our endpoint knows about the OTHER endpoint.
+clientsocket, address = s.accept()
+print(f"connection from {address} has been established")
+
+
+userInput = clientsocket.recv(1024).decode()
+
+if str(userInput) in userDict:
+	print("userexist") 
+	data = "ENter password"
+	clientsocket.send(data.encode())
+	
+passInput = clientsocket.recv(1024).decode()
+
+# if the user has entered the correct password - login success
+if userDict[str(userInput)] == str(passInput):
+	data = "LOGIN SUCCESSFUL"
+	clientsocket.send(data.encode())
+
+	
